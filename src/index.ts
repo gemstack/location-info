7f7fd66f3847c112ee7225e952f690e550cf7673
gemstack18 <gemstack18@gmail.com>
2020-04-04 14:42:50 +0530

    Added functinality to get weather and current time for location

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { getLocationsFromProcessArguements } from './utils';
+import { getLocationsFromProcessArguements } from './utils'
+import { getWeatherDataForMultipleLocations } from './weather'
+import { getCurrentTime } from './currentTime'
 
-const main = () => {
-  const arguementsArray = process.argv.slice(2);
-  const locations = getLocationsFromProcessArguements(arguementsArray);
+const main = async () => {
+  const arguementsArray = process.argv.slice(2)
+  const locations = getLocationsFromProcessArguements(arguementsArray)
+  const data = await getWeatherDataForMultipleLocations(locations)
+  const dataWithCurrentTime = data.map(city => ({ ...city, currentTime: getCurrentTime(city.timezone)}))
+  console.log(dataWithCurrentTime)
+}
 
-  console.log(locations)
-};
-
-main();
+main()
